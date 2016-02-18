25b837eb66b94e9846f15410081e3966afe1a8e9
Madhura <mgadgil09@gmail.com>
2016-02-18 11:45:52 -0500

    minor code fix

--- fergus_propagation.py
+++ fergus_propagation.py
@@ -22,7 +22,7 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
         knn: Creates RBF kernel with connections to closest n_neighbors points,
             measured in euclidean distance.
     k : integer > 0
-        Number of eigenvectors to use (default: # of labels, not including 
+        Number of eigenvectors to use (default: # of labels, not including
         unlabeled data) in eigen-decomposition.
     gamma : float > 0
         Parameter for RBF kernel (modes: rbf, img) (default: 20).
@@ -40,11 +40,10 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
 
     References
     ----------
-    Rob Fergus, Yair Weiss, Antonio Torralba. Semi-Supervised Learning in 
+    Rob Fergus, Yair Weiss, Antonio Torralba. Semi-Supervised Learning in
     Gigantic Image Collections (2009).
     http://eprints.pascal-network.org/archive/00005636/01/ssl-1.pdf
     '''
-
     def __init__(self, kernel = 'rbf', k = -1, gamma = 20, n_neighbors = 7, lagrangian = 10, img_dims = (-1, -1)):
         # This doesn't iterate at all, so the parameters are very different
         # from the BaseLabelPropagation parameters.
@@ -54,13 +53,12 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
         self.n_neighbors = n_neighbors
         self.lagrangian = lagrangian
         self.img_dims = img_dims
-
     def fit(self, X, y):
         '''
         Fit a semi-supervised eigenfunction label propagation model.
 
         All input data is provided in matrix X (labeled and unlabeled), and
-        corresponding label vector y with dedicated marker value (-1) for 
+        corresponding label vector y with dedicated marker value (-1) for
         unlabeled samples.
 
         Parameters
@@ -76,11 +74,9 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
         self : An instance of self.
         '''
         self.X_ = X
-
         # Construct the graph laplacian from the graph matrix.
         W = self._get_kernel(self.X_)
         L = self._normalized_graph_laplacian(W)
-
         # Perform the eigen-decomposition.
         vals = None
         vects = None
@@ -89,14 +85,12 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
             classes = np.delete(classes, 0) # remove the -1 from this list
             if self.k == -1:
                 self.k = np.size(classes)
-
         if scipy.sparse.isspmatrix(L):
             vals, vects = SLA.eigsh(L, k = self.k, which = 'LM', maxiter = 1000)
         else:
             M = L.shape[0]
             vals, vects = LA.eigh(L, eigvals = (M - self.k, M - 1))
         self.u_ = vals
-
         # Construct some matrices.
         # U: k eigenvectors corresponding to smallest eigenvalues. (n_samples by k)
         # S: Diagonal matrix of k smallest eigenvalues. (k by k)
@@ -106,13 +100,12 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
         V = np.diag(np.zeros(np.size(y)))
         labeled = np.where(y != -1)
         V[labeled, labeled] = self.lagrangian
-
         # Solve for alpha and use it to compute the eigenfunctions, f.
         A = S + np.dot(np.dot(self.U_.T, V), self.U_)
         b = np.dot(np.dot(self.U_.T, V), y)
         alpha = LA.solve(A, b)
         f = np.dot(self.U_, alpha)
-
+        f = f.reshape((f.shape[0],-1))
         # Set up a GMM to assign the hard labels from the eigenfunctions.
         g = mixture.GMM(n_components = np.size(classes))
         g.fit(f)
@@ -120,10 +113,8 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
         means = np.argsort(g.means_.flatten())
         for i in range(0, np.size(self.labels_)):
             self.labels_[i] = np.where(means == self.labels_[i])[0][0]
-
         # Done!
         return self
-
 	def predict(self, X):
 		'''
         Performs inductive inference across the model.
@@ -139,7 +130,6 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
         '''
         probas = self.predict_proba(X)
         return self.labels_[np.argmax(probas, axis=1)].ravel()
-
 	def predict_proba(self, X):
 		'''
         Predict probability for each possible outcome.
@@ -176,7 +166,6 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
         normalizer = np.atleast_2d(np.sum(probabilities, axis=1)).T
         probabilities /= normalizer
         return probabilities
-
     def _get_kernel(self, X, y = None):
         if self.kernel == "rbf":
             if y is None:
@@ -196,75 +185,64 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
         else:
             raise ValueError("%s is not a valid kernel. Only rbf and knn"
                              " are supported at this time" % self.kernel)
-
     def _img_rbf_kernel(self, X):
         A = np.diag(np.ones(X.shape[0]))
         index = 0
         for i in range(0, self.img_dims[1]):
             for j in range(0, self.img_dims[0]):
-
                 if i - 1 >= 0:
                     # Directly above.
                     other = index - self.img_dims[0]
                     rbf = pairwise.rbf_kernel(X[index], X[other], gamma = self.gamma)
                     A[index, other] = rbf
                     A[other, index] = rbf
-
                 if i + 1 < self.img_dims[1]:
                     # Directly below.
                     other = index + self.img_dims[0]
                     rbf = pairwise.rbf_kernel(X[index], X[other], gamma = self.gamma)
                     A[index, other] = rbf
                     A[other, index] = rbf
-
                 if j - 1 >= 0:
                     # Directly to the left.
                     other = index - 1
                     rbf = pairwise.rbf_kernel(X[index], X[other], gamma = self.gamma)
                     A[index, other] = rbf
                     A[other, index] = rbf
-
                 if j + 1 < self.img_dims[0]:
                     # Directly to the right.
                     other = index + 1
                     rbf = pairwise.rbf_kernel(X[index], X[other], gamma = self.gamma)
                     A[index, other] = rbf
                     A[other, index] = rbf
-
                 if i - 1 >= 0 and j - 1 >= 0:
                     # Upper left corner.
                     other = index - self.img_dims[0] - 1
                     rbf = pairwise.rbf_kernel(X[index], X[other], gamma = self.gamma)
                     A[index, other] = rbf
                     A[other, index] = rbf
-
                 if i - 1 >= 0 and j + 1 < self.img_dims[0]:
                     # Upper right corner.
                     other = index - self.img_dims[0] + 1
                     rbf = pairwise.rbf_kernel(X[index], X[other], gamma = self.gamma)
                     A[index, other] = rbf
                     A[other, index] = rbf
-
                 if i + 1 < self.img_dims[1] and j - 1 >= 0:
                     # Lower left corner.
                     other = index + self.img_dims[0] - 1
                     rbf = pairwise.rbf_kernel(X[index], X[other], gamma = self.gamma)
                     A[index, other] = rbf
                     A[other, index] = rbf
-
                 if i + 1 < self.img_dims[1] and j + 1 < self.img_dims[0]:
                     # Lower right corner.
                     other = index + self.img_dims[0] + 1
                     rbf = pairwise.rbf_kernel(X[index], X[other], gamma = self.gamma)
                     A[index, other] = rbf
                     A[other, index] = rbf
-
                 index += 1
         return scipy.sparse.csc_matrix(A)
-
     def _normalized_graph_laplacian(self, A):
         '''
-        Calculates the normalized graph laplacian, as the sklearn utility 
+        Calculates the normalized graph laplacian, as the sklearn utility
         graph_laplacian(normed = True) does not seem to do this.
 
         Parameters
@@ -279,7 +257,6 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
         '''
         L = None
         if scipy.sparse.isspmatrix(A):
-
             # SKLEARN
             n_nodes = A.shape[0]
             if not A.format == 'coo':
@@ -294,7 +271,6 @@ class FergusPropagation(BaseEstimator, ClassifierMixin):
             d = np.sqrt(L.sum(axis = 0))
             L /= d
             L /= d[:, np.newaxis]
-
             # The last line effectively transposes the vector d, swapping
             # the rows and columns of the division operation. Pretty clever,
             # scikit-learn!
