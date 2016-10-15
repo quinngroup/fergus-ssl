ec6ac8fbb4d0b9fa9355cc30984d843ada1bfd9a
Madhura <mgadgil09@gmail.com>
2016-10-15 14:51:37 -0400

    changes in setup

--- LabelPropagationDistributed/setup.py
+++ LabelPropagationDistributed/setup.py
@@ -1,17 +1,18 @@
-import os
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+config = {
+    'name' : "LabelPropagationDistributed",
+    'author' : "Madhura Gadgil",
+    'author_email' : "mgadgil09@gmail.com",
+    'description'  "A semi-supervised approach for clustering "
+                                   "using Label Propagation.",
+    'url' : "https://github.com/quinngroup/fergus-ssl.git",
+    'version' : "0.1",
+    'install_requires': ['pytest'],
+    'packages':['LabelPropagationDistributed'],
+    }
 
-setup(
-    name = "LabelPropagationDistributed",
-    author = "Madhura Gadgil",
-    author_email = "mgadgil09@gmail.com",
-    description = ("A semi-supervised approach for clustering "
-                                   "using Label Propagation."),
-    #url = "http://packages.python.org/an_example_pypi_project",
-    packages=['LabelPropagationDistributed', 'tests'],
-    long_description=read('README'),
-    
-)
+setup(**config)
