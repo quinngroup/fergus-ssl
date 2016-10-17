aee0edaed3909e8809747daf1ef7aaa5194587c6
mgadgil09 <mgadgil09@gmail.com>
2016-10-17 10:01:02 -0400

    Update setup.py

--- LabelPropagationDistributed/setup.py
+++ LabelPropagationDistributed/setup.py
@@ -7,7 +7,7 @@ config = {
     'name' : "LabelPropagationDistributed",
     'author' : "Madhura Gadgil",
     'author_email' : "mgadgil09@gmail.com",
-    'description'  "A semi-supervised approach for clustering "
+    'description' : "A semi-supervised approach for clustering "
                                    "using Label Propagation.",
     'url' : "https://github.com/quinngroup/fergus-ssl.git",
     'version' : "0.1",
